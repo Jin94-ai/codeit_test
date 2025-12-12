7901707a2c1394a43ac7a1fcdd5297e970f2e0ea
bykim0119 <bukim0119@gmail.com>
2025-12-12 11:47:03 +0900

    models added

---
+++ scripts/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+PY=python3
+export KAGGLE_CONFIG_DIR=./data/.kaggle
+
+# 필요한 패키지 설치
+$PY -m pip install --upgrade pip
+$PY -m pip install ultralytics==8.3.235
+$PY -m pip install kaggle==1.7.4.5
+$PY -m pip install matplotlib
+$PY -m pip install seaborn
+
+# 기본 실행 모델
+MODEL_FILE=${1:-baseline.py}
+
+# 상대경로 실행 (프로젝트 루트 기준)
+$PY -m src.data.data_load.data_loader
+$PY -m src.data.yolo_dataset.yolo_export
+$PY -m src.models.$(basename $MODEL_FILE .py)
